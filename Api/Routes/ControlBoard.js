3acf2d96805a18febc7a53e06ed8c9e441df589e
Abdelrahman Hossam <geo.abdelrahmanem@gmail.com>
2022-12-11 22:33:33 +0200

    Update

---
+++ Api/Routes/ControlBoard.js
@@ -0,0 +1,154 @@
+const express = require("express")
+const router = express.Router()
+const multer = require("multer")
+const Item = require("../Models/Item")
+const Category = require("../Models/Category")
+////////////////////multer to uplode image////////////////////////
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "image")
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now() + file.originalname)
+  },
+})
+const multi_upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype == "image/png" ||
+      file.mimetype == "image/jpg" ||
+      file.mimetype == "image/jpeg"
+    ) {
+      cb(null, true)
+    } else {
+      cb(null, false)
+      const err = new Error("Only .png, .jpg and .jpeg format allowed!")
+      err.name = "ExtensionError"
+      return cb(err)
+    }
+  },
+})
+///////////////// create Cateory /////////////////////
+router.post("/createCategory",multi_upload.single("image"),
+  async (req, res) => {
+    try {
+      let image = req.file.path
+      req.body.image = image
+      await Category.create(req.body, function (err, data) {
+        if (err) {
+          res.send({ massege: "Add failed", success: false, Data: err })
+        } else {
+          res.send({ massege: "Added successfully", success: true, Data: data })
+        }
+      })
+    } catch (err) {
+      res.send(err)
+    }
+  }
+)
+//////////////////create Item///////////////////
+router.post("/createItem", multi_upload.single("image"), async (req, res) => {
+    try {
+      let image = req.file.path
+      req.body.image = image
+      await Item.create(req.body, function (err, data) {
+        if (err) {
+          res.send({ massege: "Add failed", success: false, Data: err })
+        } else {
+          res.send({ massege: "Added successfully", success: true, Data: data })
+        }
+      })
+    } catch (err) {
+      res.send(err)
+    }
+  })
+/////////////////////// get Item for control board/////////////////////////////
+router.get("/getItem/:catId", async (req, res) => {
+    try {
+        await Item.find({ categoryId: req.params.catId }).then((data, err) => {
+        if (err) {
+            res.send({ massege: "get data failed", success: false, Data: err })
+        } else {
+          res.send({
+            massege: "get data successfully",
+            success: true,
+            Data: data,
+          })
+        }
+      })
+    } catch (err) {
+      res.send(err)
+    }
+  })
+////////////////////delete Category////////////////////
+router.delete("/deleteCategory/:categoryId", async (req, res) => {
+    try {
+      await Category.deleteOne({ _id: req.params.itemId }).then((data, err) => {
+        if (err) {
+          res.send({ massege: "delete data failed", success: false })
+        } else {
+            Item.deleteMany({categoryId:req.params.itemId}).then((data2,err2)=>{
+                res.send({ massege: "delete data successfully", success: true })
+            })
+        }
+      })
+    } catch (err) {
+      res.send(err)
+    }
+  })
+////////////////////delete Item ////////////////////
+router.delete("/deleteItem/:itemId", async (req, res) => {
+    try {
+      await Item.deleteOne({ _id: req.params.itemId }).then((data, err) => {
+        if (err) {
+          res.send({ massege: "delete data failed", success: false })
+        } else {
+          res.send({ massege: "delete data successfully", success: true })
+        }
+      })
+    } catch (err) {
+      res.send(err)
+    }
+  })
+//////////////////////update Item Status/////////////////////////
+router.post("/updateItemStatus/:itemId", async (req, res) => {
+    try {
+      await Item.findOne({ _id: req.params.itemId }).then((data, err) => {
+        if (err) {
+          res.send({ massege: "delete data failed", success: false, Data: err })
+        } else {
+          data.status = "inactive"
+          data.save()
+          res.send({
+            massege: "delete data successfully",
+            success: true,
+            Data: data,
+          })
+        }
+      })
+    } catch (err) {
+      res.send(err)
+    }
+  })
+///////////////////////update  Item ////////////////////////////
+// router.post("/updateItem/:itemId",async(req,res)=>{
+//     try{
+//         await Item.findOne({"_id":req.params.itemId}).then((data,err)=>{
+//             if(err){
+//                 res.send({massege: "delete data failed", success: false,Data:err});
+//             }else{
+//                 data.price = "inactive"
+//                 data.save();
+//                 res.send({massege: "delete data successfully", success: true,Data:data});
+//             }
+//         });
+//     }catch(err){
+//         res.send(err);
+//     }
+// })
+///////////////////////////////////////////////////
+module.exports = router
+
+
+
---
+++ Api/Routes/Menu.js
@@ -0,0 +1,46 @@
+const express = require("express")
+const router = express.Router()
+const Item = require("../Models/Item")
+const Category = require("../Models/Category")
+////////////////////////////////////////////
+
+///////////////////get All Category//////////
+router.get("/getCategory", async (req, res) => {
+  try {
+    await Category.find({}).then((data, err) => {
+      if (err) {
+        res.send({ massege: "get data failed", success: false, Data: err })
+      } else {
+        res.send({
+          massege: "get data successfully",
+          success: true,
+          Data: data,
+        })
+      }
+    })
+  } catch (err) {
+    res.send(err)
+  }
+})
+//////////////////get all item in the same category id//////////////////////
+router.get("/getItem/:catId", async (req, res) => {
+  try {
+    await Item.find({
+      $and: [{ status: "active" }, { categoryId: req.params.catId }],
+    }).then((data, err) => {
+      if (err) {
+        res.send({ massege: "get data failed", success: false, Data: err })
+      } else {
+        res.send({
+          massege: "get data successfully",
+          success: true,
+          Data: data,
+        })
+      }
+    })
+  } catch (err) {
+    res.send(err)
+  }
+})
+///////////////////////////////////////////////////
+module.exports = router
