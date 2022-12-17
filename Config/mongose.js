f8b58addd8fa573969a299e45773a6815d1ac483
Abdelrahman Hossam <geo.abdelrahmanem@gmail.com>
2022-12-17 22:02:30 +0200

    strictQuery

--- Config/mongose.js
+++ Config/mongose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 mongoose.connect('mongodb+srv://abdelrahman:mVOSes850ZJFl95R@cluster0.kpcj9of.mongodb.net/?retryWrites=true&w=majority').then((x)=>
 {
     console.log("connection is open");
